07dc44e1a42af9dfe5e144349cdcd51f95bf7468
splegi <andreyyarilov2996@gmail.com>
2025-09-23 12:43:50 +0300

    Добавлен рабочий тест для главной страницы

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,18 @@
 from app import app
 
 def test_index():
+    # Создаем тестовый клиент Flask
     client = app.test_client()
+    
+    # Делаем GET-запрос к главной странице
     response = client.get('/')
+    
+    # Проверяем, что статус-код 200 OK
     assert response.status_code == 200
-    assert b"Hello, World!" in response.data
+    
+    # Декодируем байты в строку
+    html = response.data.decode("utf-8")
+    
+    # Проверяем, что в HTML содержатся нужные элементы
+    assert "Привет, Jenkins!" in html
+    assert "Это простое Flask-приложение для тренировки CI/CD пайплайнов." in html
